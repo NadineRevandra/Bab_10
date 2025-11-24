d1ed14f1681b4b04848db32babd3768b87d659b2
NadineRevandra <nadinerevandraakilaputri@mail.ugm.ac.id>
2025-11-24 17:44:30 +0700

    aktivitas 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,46 +1,46 @@
 #include <iostream>
-#include <fstream>
 using namespace std;
 
-int cari_sekuensial(int data[], int s, int cari){
-	bool ketemu;
-	int i = 0;
-	
-	ketemu = false;
-	while ((i < s) && !(ketemu)) {
-		ketemu = (data[i++] == cari);
-	}
-	
-	if (ketemu){
-		return i;
-	} else {
-		return -1 * i;
-	}
+int cari_sekuensial(int data[], int s, int cari) {
+    bool ketemu;
+    int i = 0;
+
+    ketemu = false;
+    while ((i < s) && !(ketemu)) {
+        ketemu = (data[i++] == cari);
+ }
+ 
+if (ketemu) {
+    return i;
+} else {
+    return -1 * i;
+ }
 }
 
 int main(){
-	int data[10];
-	int n; 
-	
-	ifstream file("pencarian.txt");
-	if(file){
-	cout<<"File sukses dibuka!"<< endl;
-	int i = 0;
-	do{
-		file << data[i];
-		i++;
-	}while(file){
-		n = i;
-		
-		file.close();
-	}else{
-		cout<<"File gagal dibuka!"<< endl;
-		return 1;
-	}
-	int cari;
-	cout << "Masukkan bilangan yang dicari: ";
-	cin >> cari;
-	
-	int cari_sekuensial()
-	}
+    int n;
+
+    cout << "Masukkan jumlah data: ";
+    cin >> n;
+
+    int data[n];
+
+    cout << "Masukkan isi data:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> data[i];
+    }
+  
+    int cari;
+        cout << "Masukkan angka yg dicari: ";
+        cin >> cari;
+
+    // fungsi pencarian
+    int hasil = cari_sekuensial(data, 5, cari);
+    cout << "Ketemu di langkah: " << hasil << endl; 
+
+    return 0;
 }
+
+// nanti hasilnya gini
+// input: masukkan angka yg dicari...
+// ouput: ketemu di langkah...
